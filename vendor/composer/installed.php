d1667d032ceb38a426893d7717a84d4aff007a21
simbakl <simbakl@users.noreply.github.com>
2026-03-27 20:41:29 +0000

    deploy: production build from f9b9995b3961a2c3e92193e74c85a9226434ea0d f9b9995b3961a2c3e92193e74c85a9226434ea0d

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-production',
         'version' => 'dev-production',
-        'reference' => 'e5f3cbfe5eb0ea5d8f339b3737c8be9b78827e2c',
+        'reference' => 'f9b9995b3961a2c3e92193e74c85a9226434ea0d',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -544,7 +544,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-production',
             'version' => 'dev-production',
-            'reference' => 'e5f3cbfe5eb0ea5d8f339b3737c8be9b78827e2c',
+            'reference' => 'f9b9995b3961a2c3e92193e74c85a9226434ea0d',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
